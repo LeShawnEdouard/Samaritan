d2e4ebd87ec8994da1ab2112be3e14d70e9d0c88
LeShawn Edouard <leshawnmedouard@gmail.com>
2019-06-30 19:40:40 -0400

    Installed react-router-dom to route to our pages. Added stylesheet for bootstrap. Installed styled-components. Created and imported routes that links to different pages

--- samaritan/src/App.js
+++ samaritan/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Home } from './components/Home';
+import { Account } from './components/Account';
+import { Login } from './components/Login';
+import { NoMatch } from './components/NoMatch';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <Switch>
+            <Router exact path="/" component={Home} />
+            <Router path="/login" component={Account} />
+            <Router path="/account" component={Login} />
+            <Router component={NoMatch} />
+          </Switch>
+        </Router>
+      </React.Fragment>
+    );
+  }  
 }
 
 export default App;
--- samaritan/src/components/Login.js
+++ samaritan/src/components/Login.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export const Home = () => (
+export const Login = () => (
   <div>
-  <h2>Home Page</h2>
+  <h2>Login Page</h2>
   <p>This is a test to see if this will display on the page</p>
   <p>This is a test to see if this will display on the page</p>
   <p>This is a test to see if this will display on the page</p>
