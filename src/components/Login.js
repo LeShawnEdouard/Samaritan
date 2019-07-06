6e200bdfbfbc6793f42b1fd7555c01ae31d67257
Heather Peri <heatherhperi@gmail.com>
2019-07-06 10:33:06 -0400

    updated volunteer page

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,3 +10,4 @@ export const Login = () => (
   )
 
   export default Login;
+ 
---
+++ src/components/Volunteer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Volunteer = () => (
+    <div>
+    <h2>Volunteer Page</h2>
+ 
+    </div>
+  )
+
+  export default Volunteer;
