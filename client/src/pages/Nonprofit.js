37f44798e75bb09160d1221f28fd9365bbab603e
Heather Peri <heatherhperi@gmail.com>
2019-07-13 15:55:02 -0400

    rerouted folders, more changes to database

---
+++ client/src/pages/Nonprofit.js
@@ -0,0 +1,147 @@
+import React, { Component } from 'react';
+import Hero from '../components/Hero';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+class Nonprofit extends Component {
+  state ={
+    nonprofits: [],
+    eventName: "",
+    eventHost: "",
+    eventTime: "",
+    eventLocation: "",
+    eventAbout: ""
+  };
+
+  componentDidMount(){
+    this.loadEvents();
+  };
+
+  loadEvents = () => {
+    fetch("/api/events").then(res => console.log(res.json))
+    //API.getEvents()
+    // .then(res =>
+    //   console.log(res)
+    // )
+    // .catch( err => console.log(err))
+  };
+
+  createEvent = () => {
+    console.log("create event working" );
+    
+
+
+  };
+
+  handleInputChange = event => {
+		const { name, value } = event.target;
+		this.setState({
+			[name]: value
+	});
+};
+
+
+// function Nonprofit() {
+  render(){
+  return (
+    <div>
+      <Hero backgroundImage="https://images.unsplash.com/photo-1516880711640-ef7db81be3e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80">
+        <h1>Welcome Back!</h1>
+        <h2>Post an event or send an alert to your local volunteers!</h2>
+        <br></br>
+        <br></br>
+      </Hero>
+      <Container style={{ marginTop: 30 }}>
+        <Row>
+          <Col size="md-12">
+          </Col>
+        </Row>
+        <Row>
+          <Col size="sm-3">
+            <div>
+
+              <h1>Create Event</h1>
+              <Form>
+              <Form.Group controlId="formBasicEmail" 
+              onChange={this.handleInputChange}>
+              <Form.Label>Event Name</Form.Label>
+              <Form.Control type="email" placeholder="Enter event" name= "eventName"/>
+              <Form.Text className="text-muted">
+              </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId="formBasicEmail"
+              onChange={this.handleInputChange}>
+              <Form.Label>Host/Company</Form.Label>
+              <Form.Control type="email" placeholder="Enter host" name= "eventHost"/>
+              <Form.Text className="text-muted">
+              </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId="formBasicEmail"
+              onChange={this.handleInputChange}>
+              <Form.Label>Time of Event</Form.Label>
+              <Form.Control type="email" placeholder="Enter time" name= "eventTime"/>
+              <Form.Text className="text-muted">
+              </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId="formBasicEmail"
+              
+              onChange={this.handleInputChange}>
+              <Form.Label>Location</Form.Label>
+              <Form.Control type="email" placeholder="Enter location" name= "eventLocation"/>
+              <Form.Text className="text-muted">
+              </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId="exampleForm.ControlTextarea1"
+              
+              onChange={this.handleInputChange}>
+              <Form.Label>Tell us more about your event!</Form.Label>
+              <Form.Control as="textarea" rows="3" name= "eventAbout"/>
+              </Form.Group>
+              <Button variant="primary" onClick={() => this.createEvent()}>
+              Submit
+              </Button>
+            </Form>
+            </div>
+            </Col>
+            <Col size="sm-3">
+            <h3>Event Display Here</h3>
+          <p>
+            Event <br></br>
+            Location <br></br>
+            Time<br></br>
+          </p>
+          <p>
+            Event <br></br>
+            Location <br></br>
+            Time<br></br>
+          </p>
+          <p>
+            Event <br></br>
+            Location <br></br>
+            Time<br></br>
+          </p>
+          <p>
+            Event <br></br>
+            Location <br></br>
+            Time<br></br>
+          </p>
+            </Col>
+            <Col size="sm-3">
+            <p>
+            <button type="button" className="btn btn-primary btn-lg">Text an Event!</button>
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}}
+    
+    export default Nonprofit;
--- server.js
+++ server.js
@@ -1,18 +1,60 @@
-const http = require('http');
-const express = require('express');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+// const http = require('http');
+// const express = require('express');
+// const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
+// const app = express();
+
+// app.post('/sms', (req, res) => {
+//   const twiml = new MessagingResponse();
+
+//   twiml.message('The Robots are coming! Head for the hills!');
+
+//   res.writeHead(200, {'Content-Type': 'text/xml'});
+//   res.end(twiml.toString());
+// });
+
+// http.createServer(app).listen(1337, () => {
+//   console.log('Express server listening on port 1337');
+// });
+// Dependencies
+
+const express = require("express");
+var mysql = require("mysql");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+const PORT = process.env.PORT || 3001;
 const app = express();
+var router = express.Router();
 
-app.post('/sms', (req, res) => {
-  const twiml = new MessagingResponse();
 
-  twiml.message('The Robots are coming! Head for the hills!');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
+// var index = require ('../utils/API');
+
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  // port: 3306,
+  user: "root",
+  password: "Forever21!",
+  database: "events"
+
+});
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+  }
+  // comingUp();
 });
 
-http.createServer(app).listen(1337, () => {
-  console.log('Express server listening on port 1337');
-});
+app.use("/api", require('./utils/API')(connection, router));
+
+app.listen(PORT, () => {
+  console.log("app running on port")
+})
+
+
+
+
