b665e53f0e2b18fcf83c48cbb4c39d7a9af8f211
Heather Peri <heatherhperi@gmail.com>
2019-07-15 20:18:07 -0400

    added twilio

--- client/src/pages/Volunteer.js
+++ client/src/pages/Volunteer.js
@@ -1,21 +1,19 @@
 import React from 'react';
+// import Calendar from 'react-calendar';
 import axios from 'axios';
 // import { Container, Row, Col } from 'reactstrap';
 // import { URLSearchParams } from 'url';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-<<<<<<< HEAD
+import Table from 'react-bootstrap/Table';
+import Hero from '../components/Hero';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 // import './smsform.css';
 import Container from 'react-bootstrap/Container';
-// import sendMessage from "../send_sms"
-import Hero from '../components/Hero';
-=======
-import Table from 'react-bootstrap/Table';
-import '../App.css';
->>>>>>> origin
+
+
 
 
 class Volunteer extends React.Component {
@@ -29,6 +27,7 @@ class Volunteer extends React.Component {
     users: [],
     isLoading: true,
     errors: null
+
   };
 
   componentDidMount() {
@@ -95,29 +94,17 @@ class Volunteer extends React.Component {
   handleShow = () => {
     this.setState({ show: true });
   }
-
   render() {
     return (
-<<<<<<< HEAD
-    <div>
-  
-      <React.Fragment>
-      <Hero backgroundImage="https://images.unsplash.com/photo-1487687944474-d9cf58dda287?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=732&q=80">
+
+<div>
+<Hero backgroundImage="https://images.unsplash.com/photo-1487687944474-d9cf58dda287?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=732&q=80">
 
       </Hero>
-=======
-
->>>>>>> origin
-<div>
-  
           <Row>
-<<<<<<< HEAD
-          <Col size="md-4">
-           <br></br>
-=======
           <Col className="col-md-6">
+            <br></br>
 
->>>>>>> origin
         <h4>Non-Profit List</h4>
         <br></br>
         <div className="scroll-table">
@@ -133,36 +120,6 @@ class Volunteer extends React.Component {
           {
             this.state.users.map(user => {
               return (
-<<<<<<< HEAD
-                <Container>
-                <h6>
-                <div key={user.charityName}>
-                  <p>{user.charityName}</p>
-                  <p>{user.mailingAddress.city}, {user.mailingAddress.stateOrProvince}</p>
-                  <div>
-                  <a href={user.charityNavigatorURL} onClick={this.handleClick}>Learn More Here!</a>
-                     {/* <p>{user.charityNavigatorURL}</p> */}
-                  </div>
-                  {/* <p>{user.mailingAddress.city}, {user.mailingAddress.stateOrProvince}</p> */}
-                </div>
-
-                <br></br>
-                <br></br>
-                </h6>
-                </Container>
-              );
-            })
-          ) : 
-          (
-            <p>Loading...</p>
-          )}
-        </div>
-
-        </Col>
-        <Col size="md-4">
-          <br></br>
-          <h4>Upcoming Events!</h4>
-=======
                   <tr>
                     <td key={user.charityName}>{user.charityName}</td>
                     <td key={user.mailingAddress.city}>{user.mailingAddress.city}, {user.mailingAddress.stateOrProvince}</td>
@@ -178,96 +135,40 @@ class Volunteer extends React.Component {
         </div>
 
         </Col>
-        <Col className="col-md-6">
+        <Col className="col-md-3">
           <Row>
             <div>
           <h3>Event Display Here</h3>
->>>>>>> origin
           <p>
-            Event: 5K <br></br>
-            Location: 100 Martin Street Raleigh, NC <br></br>
-            Time 8:00 AM<br></br>
+            Event <br></br>
+            Location <br></br>
+            Time<br></br>
           </p>
           <p>
-            Event: Clean The Water <br></br>
-            Location: Wilmington, NC <br></br>
-            Time: All Day August 1-3<br></br>
+            Event <br></br>
+            Location <br></br>
+            Time<br></br>
           </p>
           <p>
-            Event: Rebuild  <br></br>
-            Location: Historic Oakwood <br></br>
-            Time: 9:00 AM<br></br>
+            Event <br></br>
+            Location <br></br>
+            Time<br></br>
           </p>
           <p>
-            Event: Clean The Road <br></br>
-            Location: Highway 540 <br></br>
-            Time: 12 PM<br></br>
+            Event <br></br>
+            Location <br></br>
+            Time<br></br>
           </p>
-<<<<<<< HEAD
-          <br></br>
-          <h5>
-            Opt in for text reminders on  <br></br>
-            upcoming volunteer oppurtunities.<br></br>
-            We can't wait to see you!<br></br>
-          </h5>
-
-          <button type="button" className="btn btn-primary btn-lg" onClick={this.handleShow}>Event Text Opt-In!</button>
-          <Modal show={this.state.show} onHide={this.handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-                <form
-                onSubmit={this.onSubmit}
-                className={this.state.error ? 'error sms-form' : 'sms-form'}>
-            <div>
-              <label htmlFor="to">To:</label>
-              <input
-                  type="tel"
-                  name="to"
-                  id="to"
-                  value={this.state.message.to}
-                  onChange={this.onHandleChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="body">Body:</label>
-              <textarea name="body" id="body"
-              value={this.state.message.body}
-              onChange={this.onHandleChange}/>
-            </div>
-            <button type="submit">
-              Send message
-            </button>
-          </form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={this.handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-
-            </Col>
-        <Col size="md-4">
-        <br></br>
-              <div className="fb-page" data-href="https://www.facebook.com/volunteer365/" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/volunteer365/" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/volunteer365/">Volunteering</a></blockquote></div>
-        
-
-        </Col>
-
-=======
           <button type="button" class="btn btn-primary btn-lg">Event Text Opt-In!</button>
           </div>
           </Row>
+          <Col className="col-md-3">
+
           <Row>
           <div class="fb-page" data-href="https://www.facebook.com/volunteer365/" data-tabs="timeline" data-width="" data-height="250px" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/volunteer365/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/volunteer365/">Volunteering</a></blockquote></div>
           </Row>
           </Col>
->>>>>>> origin
+          </Col>
         </Row>
         </div>
 
